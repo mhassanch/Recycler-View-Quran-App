b97ce56dde51b22737c999d7afe0cb6555456361
mhassanch <58620309+mhassanch@users.noreply.github.com>
2022-09-15 09:38:20 -0700

    add: ayatAdapter

---
+++ app/src/main/java/com/hassan/quranapprv/ayatAdapter.java
@@ -0,0 +1,31 @@
+package com.hassan.quranapprv;
+
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ayatAdapter extends ArrayAdapter {
+    private final Activity Context;
+    private final ArrayList<translationModel> ayatList;
+    public ayatAdapter( Activity context, ArrayList<translationModel> ayatList) {
+        super(context, R.layout.ayat_single, ayatList);
+        Context = context;
+        this.ayatList = ayatList;
+    }
+
+    public View getView(int position, View view, ViewGroup parent){
+        LayoutInflater inflator = Context.getLayoutInflater();
+        View Single = inflator.inflate(R.layout.ayat_single,null,true);
+        TextView v =Single.findViewById(R.id.arabic);
+        TextView v1 =Single.findViewById(R.id.translationV);
+        v.setText(ayatList.get(position).getArabic_Text());
+        v1.setText(ayatList.get(position).getTranslation());
+        return Single;
+    }
+}
